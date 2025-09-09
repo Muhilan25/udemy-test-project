1ad9671b14733a90731ea1701e309d32e875283f
muhilan <muhilanrangaraj99@gmail.com>
2025-09-09 11:47:32 +0530

    3nd branch

--- devops/main.tf
+++ devops/main.tf
@@ -12,21 +12,21 @@ provider "aws" {
 }
 
 module "nexus_instance" {
-  source       = "./ec2_instance"
+  source       = "./devops/ec2_instance"
   ami          = var.aws_ami  # Example AMI ID, replace with your desired AMI
   instance_type = var.aws_type             # Example instance type, replace with your desired type
   instance_name = "nexus"
 }
 
 module "sonar_instance" {
-  source       = "./ec2_instance"
+  source       = "./devops/ec2_instance"
   ami          = var.aws_ami  # Example AMI ID, replace with your desired AMI
   instance_type = var.aws_type              # Example instance type, replace with your desired type
   instance_name = "sonar"
 }
 
 module "test_instance" {
-  source       = "./ec2_instance"
+  source       = "./devops/ec2_instance"
   ami          = var.aws_ami  # Example AMI ID, replace with your desired AMI
   instance_type = var.aws_type              # Example instance type, replace with your desired type
   instance_name = "test"
