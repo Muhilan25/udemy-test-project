c270d2e42d7c8ec5e440501189415391a159ea6f
muhilan <muhilanrangaraj99@gmail.com>
2025-09-09 11:27:48 +0530

    1st branch

---
+++ devops/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  description = "AWS region"
+  default = "ap-south-1"
+}
+
+variable "aws_type" {
+  description = "AWS ec2 type"
+  default = "t3.medium"
+}
+
+variable "aws_ami" {
+  description = "AWS ami"
+  default = "ami-0861f4e788f5069dd"
+}
